98db38a97a22288cf06ddf0103bead1a5010d705
MalyshevMS <matvey.malyshev29@gmail.com>
2024-05-16 23:06:13 +0300

    deleted comments from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,26 @@
 #include <GLFW/glfw3.h>
 
-int main(void)
-{
+int main(void) {
     GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
         return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
+    }
+        
     window = glfwCreateWindow(640, 480, "Hello Window :)", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
-        /* Render here */
         //glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
