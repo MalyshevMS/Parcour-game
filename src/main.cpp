347cc5075f0297d444b299b8ebd66f448f2f95a9
MalyshevMS <matvei.malyshev29@gmail.com>
2024-08-20 21:21:40 +0300

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 // #define debug
-// #define fullscreen
+#define fullscreen
 // #define online
 
 #include <glad/glad.h> // OpenGL libs
@@ -43,54 +43,47 @@ using namespace std;
     glm::ivec2 Size(3840, 2160);
 #endif
 
-// Eng vars
+// Engine vars
 GLenum gl_texture_mode = GL_LINEAR; // GL_LINEAR or GL_NEAREST
 string gl_default_shader = "DefaultShader"; // Name of default shader
 string gl_sprite_shader = "SpriteShader"; // Name of default sprite shader
-vector <string> gl_default_shader_path_list = {"res/shaders/vertex.cfg", "res/shaders/fragment.cfg"}; // List of paths to default shader
-vector <string> gl_sprite_shader_path_list = {"res/shaders/vSprite.cfg", "res/shaders/fSprite.cfg"}; // List of paths to sprite shader
-int gl_sprite_size = 80;
-
-vector <string> tx_path_list = {
-    "res/textures/wall.png",
-    "res/textures/player.png",
-    "res/textures/bullet.png"
-}; // List of paths to textures
+string gl_default_shader_path_list[] = {
+    "res/shaders/vertex.cfg", "res/shaders/fragment.cfg"
+}; // List of paths to default shader
+string gl_sprite_shader_path_list[] = {
+    "res/shaders/vSprite.cfg", "res/shaders/fSprite.cfg"
+}; // List of paths to sprite shader
+int gl_sprite_size = 80; // Default size of a sprite
 
 float cam_x = 0; // Camera X pos
 float cam_y = 0; // Camera Y pos
 float cam_rot = 180.f; // Camera rotation
-float cam_mag = 1.f;
-float cam_speed = 10.f;
-float cam_mag_speed = 0.01f;
-bool cam_locked = true;
+float cam_mag = 1.f; // Current camera magnifying
+float cam_speed = 10.f; // Camera movement speed
+float cam_mag_speed = 0.01f; // Camera magnifying speed
+bool cam_locked = true; // Camera lock (to player) flag
 
 int sv_max_speed = 10; // Max player movement speed
-int sv_jump_speed = 5;
-int sv_jump_height = 2 * gl_sprite_size;
-int sv_gravity = 800;
+int sv_jump_speed = 5; // Player(s) jump speed
+int sv_jump_height = 160; // Max player jump height
 
-int pl_offset_x = 40;
-int pl_offset_y = 280;
-int pl_x = Size.x / 2 - pl_offset_x;
-int pl_y = Size.y / 2 - pl_offset_y;
-int pl2_x = 0;
-int pl2_y = 0;
-bool pl_on_floor = true;
-bool pl_is_jumping = false;
-bool pl_is_spidering = false;
-vector <glm::vec2> pl_coords;
+int pl_x = Size.x / 2 - 40; // Player 1 X
+int pl_y = Size.y / 2 - 280; // Player 1 Y
+int pl2_x = 0; // Player 2 X
+int pl2_y = 0; // Player 2 Y
+bool pl_on_floor = true; // Player on floor flag
+bool pl_jumping = false; // Player jumping flag
+bool pl_spidering = false; // Player spider mode flag
 
-ResourceManager rm_main;
+ResourceManager rm_main; // Main Resource manager
 
-TexLoader tl_textures;
+TexLoader tl_textures; // Main Texture loader
 
-SprGroup sg_sprites;
-SprGroup sg_player;
-SprGroup sg_mobs;
-SprGroup sg_player2;
+SprGroup sg_sprites; // Group for obstacles, walls, etc.
+SprGroup sg_player; // Group for Player 1
+SprGroup sg_player2; // Group for Player 2
 
-Parser pars_main;
+Parser pars_main; // Main parser
 
 void sizeHandler(GLFWwindow* win, int width, int height) {
     Size.x = width;
@@ -133,19 +126,19 @@ int collides_right() {
 void jump() {
     if (pl_on_floor && !collides_ceiling()) {
         int height = pl_y + sv_jump_height;
-        pl_is_jumping = true;
+        pl_jumping = true;
         pl_on_floor = false;
         while (pl_y < height && !collides_ceiling()) {
             pl_y += sv_jump_speed;
             sleep(10);
         }
 
-        while (!collides_floor() && pl_y > 0 && !pl_is_spidering) {
+        while (!collides_floor() && pl_y > 0 && !pl_spidering) {
             pl_y -= sv_jump_speed;
             sleep(10);
         }
         pl_on_floor = true;
-        pl_is_jumping = false;
+        pl_jumping = false;
     }
 }
 
@@ -169,7 +162,7 @@ void onceKeyHandler(GLFWwindow* win, int key, int scancode, int action, int mode
 }
 
 void fall() {
-    if (!collides_floor() && !pl_is_spidering && !pl_is_jumping && pl_y > 0) pl_y -= sv_jump_speed;
+    if (!collides_floor() && !pl_spidering && !pl_jumping && pl_y > 0) pl_y -= sv_jump_speed;
 }
 
 void detect_fail() {
@@ -201,8 +194,8 @@ void keyHandler(GLFWwindow* win) {
     }
 
     if (glfwGetKey(win, KEY_W) == GLFW_PRESS && collides_ceiling()) {
-        pl_is_spidering = true;
-    } else pl_is_spidering = false;
+        pl_spidering = true;
+    } else pl_spidering = false;
 
     if (glfwGetKey(win, KEY_0) == GLFW_PRESS) {
         cam_mag -= cam_mag_speed;
@@ -235,9 +228,23 @@ void keyHandler(GLFWwindow* win) {
 }
 
 int main(int argc, char const *argv[]) {
+    if (argc >= 2 && argv[1] == "-res") {
+        if (argc < 3) {
+            cerr << "Using: -res widthxheight" << endl;
+            return -1;
+        } else {
+            string res = argv[2];
+            Size.x = stoi(res.substr(0, res.find_first_of('x')));
+            Size.y = stoi(res.substr(res.find_first_of('x') + 1));
+
+            pl_x = Size.x / 2 - 40;
+            pl_y = Size.y / 2 - 280;
+        }
+    }
+
     string comand_line;
 
-    #ifdef debug
+    #ifdef debug // Debugger console (not ready yet)
         do {
             cout << "Minimal2D > ";
             cin >> comand_line;
@@ -246,38 +253,39 @@ int main(int argc, char const *argv[]) {
         } while (comand_line != "play");
     #endif
 
-    if (!glfwInit()) {
+    if (!glfwInit()) { // Check for GLFW
         cerr << "glfwInit failed!" << endl;
         return -1;
     }
 
+    // GLFW window settings
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_MAXIMIZED, GL_TRUE);
+    glfwWindowHint(GLFW_MAXIMIZED, 0);
 
-    #ifdef fullscreen
+    #ifdef fullscreen // Creating window (fullscreen or windowed)
         GLFWwindow* window = glfwCreateWindow(Size.x, Size.y, "ParCour game", glfwGetPrimaryMonitor(), nullptr);
     #else 
         GLFWwindow* window = glfwCreateWindow(Size.x, Size.y, "ParCour game", nullptr, nullptr);
     #endif
 
-    if (!window) {
+    if (!window) { // Checking for creating window
         cerr << "glfwCreateWindow failed!" << endl;
         glfwTerminate();
         return -1;
     }
 
-    glfwSetKeyCallback(window, onceKeyHandler);
-    glfwSetWindowSizeCallback(window, sizeHandler);
+    glfwSetKeyCallback(window, onceKeyHandler); // Setting Key Handler
+    glfwSetWindowSizeCallback(window, sizeHandler); // Setting Size Handler
 
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGL()) {
+    if (!gladLoadGL()) { // Checking for GLAD
         cerr << "Can't load GLAD!" << endl;
     }
 
-    #ifdef online
+    #ifdef online // Checking for WSA (if online mode is on)
         WSAData wsaData;
         WORD DLLVersion = MAKEWORD(2, 1);
         if (WSAStartup(DLLVersion, &wsaData) != 0) {
@@ -286,108 +294,108 @@ int main(int argc, char const *argv[]) {
         }
     #endif
 
-    cout << "Renderer: " << glGetString(GL_RENDERER) << endl;
+    cout << "Renderer: " << glGetString(GL_RENDERER) << endl; // Displaying OpenGL info
     cout << "OpenGL version: " << glGetString(GL_VERSION) << endl;
 
-    glClearColor(0.19f, 0.61f, 0.61f, 1.f);
+    glClearColor(0.19f, 0.61f, 0.61f, 1.f); // Sky color (in vec4 format)
 
     {
-        rm_main = ResourceManager(argv[0]);
+        rm_main = ResourceManager(argv[0]); // Binding all classes together
         tl_textures = TexLoader(&rm_main);
         sg_sprites = SprGroup(&rm_main);
         sg_player = SprGroup(&rm_main);
         sg_player2 = SprGroup(&rm_main);
-        sg_mobs = SprGroup(&rm_main);
         pars_main = Parser(&rm_main, &tl_textures, &sg_sprites);
 
+        // Creating and checking for default shader
         auto defaultShaderProgram = rm_main.loadShaders(gl_default_shader, gl_default_shader_path_list[0], gl_default_shader_path_list[1]);
-
         if (!defaultShaderProgram) {
             cerr << "Can't create shader program!" << endl;
             return -1;
         }
 
+        // Creating and checking for sprite shader
         auto spriteShaderProgram = rm_main.loadShaders(gl_sprite_shader, gl_sprite_shader_path_list[0], gl_sprite_shader_path_list[1]);
-
         if (!spriteShaderProgram) {
             cerr << "Can't create SpriteShader" << endl;
             return -1;
         }
 
-        tl_textures.add_texture("Wall", tx_path_list[0]);
-        tl_textures.add_texture("Player", tx_path_list[1]);
-        tl_textures.add_texture("Bullet", tx_path_list[2]);
-
+        // Using default shader
         defaultShaderProgram->use();
         defaultShaderProgram->setInt("tex", 0);
 
+        // Using sprite shader
         spriteShaderProgram->use();
         spriteShaderProgram->setInt("tex", 0);
 
-        pars_main.parse_lvl("res/lvl/level.json");
+        pars_main.parse_lvl("res/lvl/level.json", &gl_sprite_size); // Parsing level
 
-        sg_player.add_sprite("Player", gl_sprite_shader, gl_sprite_size, gl_sprite_size, 0.f, pl_x, pl_y);
+        sg_player.add_sprite("Player", gl_sprite_shader, gl_sprite_size, gl_sprite_size, 0.f, pl_x, pl_y); // Adding Player 1 sprite
 
         #ifdef online
-            sg_player2.add_sprite("Player", gl_sprite_shader, gl_sprite_size, gl_sprite_size, 0.f, pl2_x, pl2_y);
+            sg_player2.add_sprite("Player", gl_sprite_shader, gl_sprite_size, gl_sprite_size, 0.f, pl2_x, pl2_y); // Adding Player 2 sprite
 
+            // Parsing serffer.cfg
             string servercfg = rm_main.getFileStr("server.cfg");
             string ip = servercfg.substr(0, servercfg.find_first_of(':'));
             unsigned short port = atoi(servercfg.substr(servercfg.find_first_of(':') + 1, servercfg.find_first_of(';')).c_str());
             
-            Client cli(ip, port);
+            Client cli(ip, port); // Creating client
         #endif
 
-        while (!glfwWindowShouldClose(window)) {
+        while (!glfwWindowShouldClose(window)) { // Main game loop
             glClear(GL_COLOR_BUFFER_BIT);
 
-            keyHandler(window);
+            keyHandler(window); // Setting key handler
 
+            // Projection matrix variables
             float projMat_right  = Size.x * cam_mag + cam_x;
             float projMat_top    = Size.y * cam_mag + cam_y;
             float projMat_left   = - Size.x * (cam_mag - 1) + cam_x;
             float projMat_bottom = - Size.y * (cam_mag - 1) + cam_y;
 
-            glm::mat4 projMat = glm::ortho(projMat_left, projMat_right, projMat_bottom, projMat_top, -100.f, 100.f);
+            glm::mat4 projMat = glm::ortho(projMat_left, projMat_right, projMat_bottom, projMat_top, -100.f, 100.f); // Setting projection matrix
 
+            // Using projection matrix
             defaultShaderProgram->setMat4("projMat", projMat);
             spriteShaderProgram->setMat4("projMat", projMat);
 
-            defaultShaderProgram->use();
-            tl_textures.bind_all();
+            defaultShaderProgram->use(); // Using default shader
+            tl_textures.bind_all(); // Binding all textures
 
-            #ifdef online
+            #ifdef online // Sending and recieving position
                 cli.send_msg(to_string(pl_x) + "/" + to_string(pl_y) + ";");
                 auto pl2_coords = cli.recv_msg();
                 pl2_x = stoi(pl2_coords.substr(0, pl2_coords.find_first_of("/")));
                 pl2_y = stoi(pl2_coords.substr(pl2_coords.find_first_of("/") + 1, pl2_coords.find_first_of(";")));
             #endif
 
-            fall();
+            fall(); // Always falling down
 
-            sg_player.rotate_all(180 - cam_rot);
-            sg_player.set_pos(pl_x, pl_y);
+            sg_player.rotate_all(180 - cam_rot); // Setting rotation (Player 1)
+            sg_player.set_pos(pl_x, pl_y); // Setting position (Player 1)
 
-            sg_player2.rotate_all(180 - cam_rot);
-            sg_player2.set_pos(pl2_x, pl2_y);
+            sg_player2.rotate_all(180 - cam_rot); // Setting rotation (Player 2)
+            sg_player2.set_pos(pl2_x, pl2_y); // Setting position (Player 2)
 
+            // Rendering all sprites
             sg_sprites.render_all();
             sg_player.render_all();
-            sg_mobs.render_all();
             sg_player2.render_all();
 
-            sleep(1);
-            glfwSwapBuffers(window);
-            glfwPollEvents();
+            sleep(1); // 1ms delay
+            glfwSwapBuffers(window); // Swapping front and back buffers
+            glfwPollEvents(); // Polling events
         }
         
-        sg_sprites.delete_all();
+        // Deleting all sprites from all groups
+        sg_sprites.delete_all(); 
         sg_player.delete_all();
-        sg_mobs.delete_all();
         sg_player2.delete_all();
     }
 
 
-    glfwTerminate();
-    return 0;
+    glfwTerminate(); 
+    return 0; // Exiting programm
 }
