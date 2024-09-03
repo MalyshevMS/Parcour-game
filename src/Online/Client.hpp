3242f01dbe614a65c840ee45164c622022efbce1
MalyshevMS <matvey.malyshev29@gmail.com>
2024-09-03 20:27:34 +0300

    Updated client and CMake

--- src/Online/Client.hpp
+++ src/Online/Client.hpp
@@ -1,43 +1,37 @@
-#pragma comment(lib, "ws2_32.lib")
-#include <winsock2.h>
+#include <SFML/Network.hpp>
 #include <iostream>
 #include <string>
-#pragma warning(disable: 4996)
 
 using namespace std;
 
 class Client {
 private:
-	SOCKET Connection;
+    sf::TcpSocket socket;
+    sf::IpAddress ip;
+    sf::Packet packet;
 public:
-	Client(string ip, unsigned short port) {
-		SOCKADDR_IN addr;
-		int sizeofaddr = sizeof(addr);
-		addr.sin_addr.s_addr = inet_addr(ip.c_str());
-		addr.sin_port = htons(port);
-		addr.sin_family = AF_INET;
+    Client(string ip, unsigned short port) {
+        this->ip = sf::IpAddress(ip);
+        if (socket.connect(this->ip, port) != sf::Socket::Done) {
+            std::cout << "Error: failed connect to server.\n";
+            exit(1);
+        }
+        std::cout << "Connected!\n";
+        socket.setBlocking(false);
+    };
 
-		Connection = socket(AF_INET, SOCK_STREAM, NULL);
-		if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
-			std::cout << "Error: failed connect to server.\n";
-			exit(1);
-		}
-		std::cout << "Connected!\n";
-	};
+    string recv_msg() {
+        if (socket.receive(packet) == sf::Socket::Done) {
+            std::string msg;
+            packet >> msg;
+            return msg;
+        }
+        return string();
+    };
 
-	string recv_msg() {
-		int msg_size;
-		recv(Connection, (char*)&msg_size, sizeof(int), NULL);
-		char* msg = new char[msg_size + 1];
-		msg[msg_size] = '\0';
-		recv(Connection, msg, msg_size, NULL);
-		return string(msg);
-		delete[] msg;
-	};
-
-	void send_msg(string msg) {
-		int msg_size = msg.size();
-		send(Connection, (char*)&msg_size, sizeof(int), NULL);
-		send(Connection, msg.c_str(), msg_size, NULL);
-	};
+    void send_msg(const string& msg) {
+        packet.clear();
+        packet << msg;
+        socket.send(packet);
+    };
 };
