831e9e8ad22e28a6931f0ddc868c12a78e3fa95c
MalyshevMS <matvey.malyshev29@gmail.com>
2025-05-06 21:34:05 +0300

    fixed sfml library

--- src/Online/NetHandler.hpp
+++ src/Online/NetHandler.hpp
@@ -7,21 +7,21 @@ using namespace std;
 class NetHandler {
 private:
     sf::TcpSocket socket = sf::TcpSocket();
-    sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+    sf::IpAddress ip = sf::IpAddress::LocalHost;
     sf::Packet packet = sf::Packet();
 public:
     NetHandler(string ip, unsigned short port, bool srv = false) {
         this->packet = sf::Packet();
-        this->ip = sf::IpAddress(ip);
+        this->ip = sf::IpAddress(*sf::IpAddress::resolve(ip));
 
         if (srv) {
             sf::TcpListener listener = sf::TcpListener();
             listener.listen(port);
 
-            if (listener.accept(socket) != sf::Socket::Done) {
+            if (listener.accept(socket) != sf::Socket::Status::Done) {
                 std::cerr << "Error!\n";
             }
-        } else if (socket.connect(this->ip, port) != sf::Socket::Done) {
+        } else if (socket.connect(this->ip, port) != sf::Socket::Status::Done) {
             std::cerr << "Error: failed connect to server.\n";
             return;
         }
@@ -30,7 +30,7 @@ public:
     };
 
     string recv_msg() {
-        if (socket.receive(packet) == sf::Socket::Done) {
+        if (socket.receive(packet) == sf::Socket::Status::Done) {
             std::string msg;
             packet >> msg;
             return msg;
